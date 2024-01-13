6e774f356a30244ebdf77a9dc253300cf2f4e72a
DESKTOP-J8KSOOE\rlarl <rlarlgus300@gmail.com>
2024-01-14 04:29:08 +0900

    연도, 월 별로 구매목록을 불러오도록 하였습니다. 1. next 파일 라우팅을 이용하여 기존 paymentList파일의 도메인을 localhost:3000/paymentList로 기본 도메인 설정함 (paymentList/index.js가 기존의 paymentList) 2. paymentList/[param]에서 param에 따라 api를 호출하도록 함 (ex) paymentList?year=2024&month=1 일 경우, 2024년, 1월달에 해당하는 구매목록 리스트 불러옴) 3. 기존에 DB에서 date의 타입이 String 이었던 점을 수정하여 Date 타입으로 변경하였으며, 이에 따라 domain도 수정함 4.  paymentList?year=2024&month=1 와 같은 방식을 통해 해당하는 연도, 월에 해당하는 구매목록 리스트를  불러온 후 상단의 '구매목록' 버튼을 눌렀을 시 전체 구매목록이 불러오지지 않았던 점 수정 (기존에 상단 바가 Link였던 것을 a 태그를 이용하였음

---
+++ AutumnShop/front/Autumnshop/pages/paymentList/[param].js
@@ -0,0 +1,133 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { makeStyles } from "@mui/styles";
+import axios from "axios";
+import PaymentDate from "./paymentDate";
+import { useRouter } from "next/router";
+
+// CSS 모음
+const useStyles = makeStyles((theme) => ({
+  cartContainer: {
+    width: "800px",
+    margin: "20px",
+    padding: "20px",
+    border: "1px solid #ccc",
+    borderRadius: "8px",
+    backgroundColor: "#f8f8f8",
+  },
+
+  cartTable: {
+    width: "800px",
+  },
+  cartItem: {
+    width: "500px",
+    border: "1px solid #ddd",
+    borderRadius: "8px",
+    backgroundColor: "#fff",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)", // 그림자 효과를 변경
+    transition: "transform 0.3s ease-in-out", // 호버 효과를 위한 transition 추가
+    "&:hover": {
+      transform: "scale(1.05)", // 호버 시 약간 확대됨
+      boxShadow: "0 8px 16px rgba(0, 0, 0, 0.3)", // 호버 시 그림자 효과 증가
+    },
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  productImage: {
+    width: "100px",
+    alignSelf: "center",
+  },
+}));
+
+async function getCartItem(loginInfo, setPaymentItems) {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const year = urlSearchParams.get("year");
+  const month = urlSearchParams.get("month");
+
+  if (year != null && month != null) {
+    const paymentResponse = await axios
+      .get(`http://localhost:8080/payment/${year}/${month}`, {
+        headers: {
+          Authorization: `Bearer ${loginInfo.accessToken}`,
+        },
+      })
+      .then((paymentResponse) => {
+        setPaymentItems(paymentResponse.data);
+      });
+  } else {
+    // 1. 현재 로그인한 아이디에 따라 맞는 카트 가져옴
+    const paymentResponse = await axios
+      .get(`http://localhost:8080/payment`, {
+        headers: {
+          Authorization: `Bearer ${loginInfo.accessToken}`,
+        },
+      })
+      .then((paymentResponse) => {
+        setPaymentItems(paymentResponse.data);
+      });
+  }
+}
+
+const paymentList = () => {
+  const classes = useStyles();
+  const [paymentItems, setPaymentItems] = useState([]);
+  let totalPrice = 0;
+
+  useEffect(() => {
+    const loginInfo = JSON.parse(localStorage.getItem("loginInfo"));
+    getCartItem(loginInfo, setPaymentItems);
+  }, []);
+
+  // 카트에 저장된 아이템들의 총 가격
+  paymentItems.forEach((item) => {
+    totalPrice += item.productPrice;
+  });
+  return (
+    <div>
+      <PaymentDate classes={classes} />
+      <div className={classes.cartContainer}>
+        <h1>구매 목록</h1>
+        <table className={classes.cartTable}>
+          <thead>
+            <tr>
+              <th>번호</th>
+              <th>상품 이름</th>
+              <th>상품 가격</th>
+              <th>평점</th>
+              <th>수량</th>
+              <th>이미지</th>
+              <th>날짜</th>
+            </tr>
+          </thead>
+          <tbody className="css">
+            {paymentItems.map((item, index) => (
+              <tr key={item.id} className={classes.cartItem}>
+                <td>{index + 1}</td>
+                <td>{item.productTitle}</td>
+                <td>{item.productPrice}</td>
+                <td>{item.productRate}</td>
+                <td>{item.quantity}</td>
+                <td>
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      alt={`Product ${index + 1}`}
+                      className={classes.productImage}
+                    />
+                  )}
+                </td>
+                <td>{item.date}</td>
+              </tr>
+            ))}
+            <tr>
+              <td>총 가격 : {totalPrice}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default paymentList;
